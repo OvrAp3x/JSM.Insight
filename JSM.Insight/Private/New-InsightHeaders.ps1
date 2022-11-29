16e505bcc6347f07e3d51484a0472fb05678c1b8
Einar Stenberg <einar@stenberg.cloud>
2022-11-29 10:38:47 +0100

    fix headers for everyothing

--- JSM.Insight/Private/New-InsightHeaders.ps1
+++ JSM.Insight/Private/New-InsightHeaders.ps1
@@ -6,7 +6,7 @@ function New-InsightHeaders {
     Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function started"
 
     $Headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
-    $Headers.Add('content-type' , 'application/json')
+    $Headers.Add('content-type' , "application/json; charset=UTF-8")
     $Headers.Add('Authorization', 'Basic ' + $InsightCreds)
 
     if ($ExperimentalApi -eq $true) {
